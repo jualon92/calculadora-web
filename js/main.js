7aa94e45a625c4b79868df8585459ad57b049d2c
jualon92 <londrajuan@gmail.com>
2022-02-12 14:27:27 -0300

    alerta de 0 personas, numeros dos digitos

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ const inputsPorcentajes = document.querySelectorAll(".interface__boton")
 const inputsCalculo = document.querySelectorAll(".refresco")
 const tipAmount = document.querySelector(".tip__number")
 const totalNumero = document.querySelector(".total__numero")
-
+const botonReset = document.querySelector(".reset")
+const notificacionPersonas = document.querySelector(".notificacion-personas")
 /*
 let valorOperacion = 0 // rever, podrian utilizarse funciones puras que retorn el valor y luego pasarse a html. sin necesidad de asignarlo ?
 let numeroPersonas = 0
@@ -18,11 +19,11 @@ let bandera = true
 */
 const datoSinIngresar = () => (inputPersonas.value == false || inputBill.value == false || !hayBotonActivo())   // si no hay ingreso en input, o en calculo, o boton activo
 
-const getTotal = (valor, personas, tip) => parseInt(valor / personas) + parseInt(tip)
+const getTotal = (valor, personas, tip) =>  ((valor / personas) + (tip)).toFixed(2)
 
 const getAmount = (valor, personas, tip) => parseInt((valor * tip / 100) / personas)
 
-const getDisplay = (ele) => ele.value !== "" ? parseInt(ele.value) : 0.00
+const getDisplay = (ele) => ele.value !== "" ? parseFloat(ele.value).toFixed(2) : 0.00
 
 
 const hayBotonActivo = () => Array.from(inputsPorcentajes).some(b => b.classList.contains("activo"))  // si hay un boton con clase activo activo
@@ -32,80 +33,18 @@ const setTablero = () => {
         console.log(inputPersonas.value)
         tipAmount.innerHTML = getAmount(getDisplay(inputBill), getDisplay(inputPersonas), eleccionPorcentaje)
         totalNumero.innerHTML = getTotal(getDisplay(inputBill), getDisplay(inputPersonas), parseInt(tipAmount.innerHTML))
+        botonReset.classList.add("reset-activo")
+        botonReset.disabled = false
 
-        //   advertencia.classList.remove("visible")
     } else { // si falta un dato,resultados deberian ser 0 para no confundir
         tipAmount.innerHTML = 0
         totalNumero.innerHTML = 0
 
-        // advertencia 
-        //   advertencia.classList.add("visible")
-        console.warn("0 personas no puede calcularse")
-        console.log(inputPersonas.value)
-        console.log(valorOperacion, numeroPersonas, eleccionPorcentaje)
 
+        botonReset.classList.remove("reset-activo")
+        botonReset.disabled = true
     }
 }
-
-
-
-
-
-/*
-function getDisplay(ele){
-
-
-    if (ele.value !== "") { //logica repetida, rever 
-        return  parseInt(ele.value) // "01" => 1
-     //   console.log("current valor operacion " + valorOperacion)
-    } else { // NaN
-        console.log("string")
-        return 0.00
-    }
-}*/
-
-
-
-/// IN 
-inputBill.addEventListener("input", e => {
-    //  e.preventDefault()
-
-    valorOperacion = getDisplay(inputBill)
-    /*
-    if (inputBill.value !== "") { //logica repetida, rever 
-        valorOperacion = parseInt(inputBill.value) // "01" => 1
-        console.log("current valor operacion " + valorOperacion)
-    } else { // NaN
-        console.log("nan  bill")
-        valorOperacion = 0
-        tipAmount.innerHTML = 0.00
-        totalNumero.innerHTML = 0.00
-    }
-    */
-
-}
-)
-
-
-inputPersonas.addEventListener("input", e => {
-    //  e.preventDefault()
-
-    numeroPersonas = getDisplay(inputPersonas)
-
-    /*
-    if (inputPersonas.value !== "") {
-        numeroPersonas = parseInt(inputPersonas.value) // "01" => 1
-        console.log("current valor operacion " + numeroPersonas)
-    } else { // NaN
-        console.log("nan  bill")
-        numeroPersonas = 0
-        tipAmount.innerHTML = 0.00
-        totalNumero.innerHTML = 0.00
-    }
-    console.log("numero de personas actual " + numeroPersonas)
-    */
-}
-)
 
 
 //tema de botones clase de %
@@ -125,7 +64,7 @@ for (const input of inputsPorcentajes) {
 }
 
 
-function apagarBotones() {
+function apagarBotones() { //podria ser forEach , const a rrow
     for (const input of inputsPorcentajes) {
         input.classList.remove("activo")
     }
@@ -142,16 +81,30 @@ for (const input of inputsCalculo) { //al detectar entrada input se actualizan l
 }
 
 
+inputPersonas.addEventListener("input", e => { //si input personas se vuelve 0 deberia mostrar html advertencia
+    if (inputPersonas.value == false){ // si valor es falsy, 0, ""
+        inputPersonas.classList.add("advertencia-personas") //coloreo border
+        notificacionPersonas.classList.replace("invisible", "visible" )
+
+    }else{
+        inputPersonas.classList.remove("advertencia-personas")
+        notificacionPersonas.classList.add("visible","invisible")
+    }
+})
+
+
 
 document.querySelector(".reset").addEventListener("click", e => {
-    /*
-      valorOperacion = 0 // rever, no me gusta 
-      numeroPersonas = 0
-      eleccionPorcentaje = 0*/
-    tipAmount.innerHTML = 0
-    totalNumero.innerHTML = 0
-    inputBill.value = 0
-    inputPersonas.value = 0
-    apagarBotones()
+
+    if (botonReset.classList.contains("reset-activo")) {
+        tipAmount.innerHTML = 0
+        totalNumero.innerHTML = 0
+        inputBill.value = 0
+        inputPersonas.value = 0
+        botonReset.classList.remove("reset-activo")
+        botonReset.disabled = true
+        apagarBotones()
+    }
+
 
 })
